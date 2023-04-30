78df61399d1b6f41e43cbfb16ca09c16eb45f017
ErreZac <raphael.zaccone@unige.it>
2023-04-30 14:08:03 +0200

    fixing stuff

--- sxhkd/scripts/cycle_profiles.sh
+++ sxhkd/scripts/cycle_profiles.sh
@@ -3,9 +3,9 @@
 PROFILE="$(asusctl profile -p | awk '{print $4}')"
 
 if [ "$PROFILE" == "Quiet" ]; then
-    /home/zac/.config/nvim/profiles/profile-1.sh
+    /home/zac/.config/sxhkd/scripts/profile-1.sh
 elif [ "$PROFILE" == "Balanced" ]; then
-    /home/zac/.config/nvim/profiles/profile-2.sh
+    /home/zac/.config/sxhkd/scripts/profile-2.sh
 elif [ "$PROFILE" == "Performance" ]; then
-    /home/zac/.config/nvim/profiles/profile-0.sh
+    /home/zac/.config/sxhkd/scripts/profile-0.sh
 fi
