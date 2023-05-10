420eb7aca6ab122c40e77a5a1e9d4bd924bc48d5
ErreZac <raphael.zaccone@unige.it>
2023-05-10 11:24:08 +0200

    not enabling curves otherwise my fan explodes

--- sxhkd/scripts/profile-0.sh
+++ sxhkd/scripts/profile-0.sh
@@ -4,4 +4,4 @@ asusctl -k off;
 xrandr --output eDP-1 --brightness .5;
 asusctl profile -P Quiet
 asusctl fan-curve --mod-profile $(asusctl profile -p | awk '{print $4}') -d
-asusctl fan-curve --mod-profile $(asusctl profile -p | awk '{print $4}') --enabled true
+asusctl fan-curve --mod-profile $(asusctl profile -p | awk '{print $4}') --enabled false
