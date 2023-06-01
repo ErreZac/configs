aa77c5384b7401922f97f9d5e49bc59c6cf691f4
ErreZac <raphael.zaccone@unige.it>
2023-06-01 07:17:29 +0200

    removed useless stuff

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -60,18 +60,6 @@ require('lazy').setup({
         end,
     },
 
-    { 
-        'folke/zen-mode.nvim',
-        config = function()
-            require("zen-mode").setup {
-                window = {
-                    width = .65 -- width will be 85% of the editor width
-                }
-            }
-            vim.api.nvim_set_keymap('n', '<C-z>', ':ZenMode<CR>', {noremap = true})
-        end,
-    },
-
     {
         'luisiacc/gruvbox-baby',
         config = function()
@@ -226,7 +214,102 @@ require('lazy').setup({
         end
     },
 
-    'neovim/nvim-lspconfig',
+    {
+        "williamboman/mason.nvim",
+        build = ":MasonUpdate",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup {
+                ensure_installed = { "pyright", "texlab", "lua_ls", "rust_analyzer" },
+            }
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config=function()
+            require('lspconfig')['clangd'].setup { }
+
+            require("lspconfig").pyright.setup{}
+
+            require('lspconfig')['rust_analyzer'].setup {
+                on_attach = on_attach,
+                settings = {
+                    ["rust-analyzer"] = {
+                        assist = {
+                            importMergeBehavior = "last",
+                            importPrefix = "by_self",
+                        },
+                        diagnostics = {
+                            disabled = { "unresolved-import" }
+                        },
+                        cargo = {
+                            loadOutDirsFromCheck = true
+                        },
+                        procMacro = {
+                            enable = true
+                        },
+                        checkOnSave = {
+                            command = "clippy"
+                        },
+                    }
+                }
+            }
+
+            require('lspconfig')['texlab'].setup {
+                capabilities = capabilities,
+                settings = {
+                    latex = {
+                        build = {
+                            onSave = true;
+                        }
+                    }
+                }
+            }
+            -- Global mappings.
+            -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+            vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+            vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+            vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+            -- Use LspAttach autocommand to only map the following keys
+            -- after the language server attaches to the current buffer
+            vim.api.nvim_create_autocmd('LspAttach', {
+                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+                callback = function(ev)
+                    -- Enable completion triggered by <c-x><c-o>
+                    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+                    -- Buffer local mappings.
+                    -- See `:help vim.lsp.*` for documentation on any of the below functions
+                    local opts = { buffer = ev.buf }
+                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+                    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                    vim.keymap.set('n', '<space>wl', function()
+                        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                    end, opts)
+                    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+                    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+                    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                    vim.keymap.set('n', '<space>f', function()
+                        vim.lsp.buf.format { async = true }
+                    end, opts)
+                end,
+            })
+
+        end
+    },
     'williamboman/nvim-lsp-installer',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-buffer',
@@ -291,53 +374,6 @@ require('lazy').setup({
             })
 
             -- Setup lspconfig.
-            local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-            require('lspconfig')['clangd'].setup {
-                capabilities = capabilities
-            }
-
-            require("lspconfig").pyright.setup{}
-            -- require'lspconfig'.pylyzer.setup{}
-
-            -- require'lspconfig'.grammarly.setup{
-            --     filetypes = { "markdown", "tex" },
-            --     clientId = "client_HCS8BeaK7zvoWinvRRNG9n"
-            -- }
-
-            require('lspconfig')['rust_analyzer'].setup {
-                on_attach = on_attach,
-                settings = {
-                    ["rust-analyzer"] = {
-                        assist = {
-                            importMergeBehavior = "last",
-                            importPrefix = "by_self",
-                        },
-                        diagnostics = {
-                            disabled = { "unresolved-import" }
-                        },
-                        cargo = {
-                            loadOutDirsFromCheck = true
-                        },
-                        procMacro = {
-                            enable = true
-                        },
-                        checkOnSave = {
-                            command = "clippy"
-                        },
-                    }
-                }
-            }
-
-            require('lspconfig')['texlab'].setup {
-                capabilities = capabilities,
-                settings = {
-                    latex = {
-                        build = {
-                            onSave = true;
-                        }
-                    }
-                }
-            }
         end
     },
 
@@ -525,5 +561,6 @@ require('lazy').setup({
             require("oil").setup()
             vim.keymap.set("n", "<leader>-", require("oil").open, { desc = "Open parent directory" })
         end
-    }
+    },
+
 })
