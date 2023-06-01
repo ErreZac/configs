8b0ffe3cdcb9b194b30751800489dfcc9b657a87
ErreZac <raphael.zaccone@unige.it>
2023-06-01 07:17:20 +0200

    found it

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -80,7 +80,7 @@ vim.opt.updatetime = 100
 local diag_float_grp = vim.api.nvim_create_augroup("DiagnosticFloat", { clear = true })
 vim.api.nvim_create_autocmd("CursorHold", {
   callback = function()
-   vim.diagnostic.open_float(nil, { focusable = true })
+   vim.diagnostic.open_float(nil, { focusable = false })
   end,
   group = diag_float_grp,
 })
