ab5e297cd19904b3fb4fd128ad5335ffd8c4c0f0
ErreZac <raphael.zaccone@unige.it>
2025-02-25 08:29:05 +0100

    minifiles

--- nvim/init.lua
+++ nvim/init.lua
@@ -127,7 +127,7 @@ require('lazy').setup({
             require("mini.completion").setup()
             require("mini.move").setup()
             require("mini.operators").setup()
-            require("mini.pairs").setup()
+            -- require("mini.pairs").setup()
             require("mini.snippets").setup()
             require("mini.splitjoin").setup()
             require("mini.surround").setup()
@@ -145,7 +145,7 @@ require('lazy').setup({
             -- require("mini.deps").setup()
             -- require("mini.diff").setup()
             -- require("mini.extra").setup()
-            -- require("mini.files").setup()
+            require("mini.files").setup()
             -- require("mini.git").setup()
             require("mini.jump").setup()
             -- require("mini.jump2d").setup()
@@ -272,6 +272,8 @@ vim.keymap.set('n', '<leader>mt', function() require("mini.map").toggle_focus() 
 
 -- mini trailspace
 vim.keymap.set('n', '<leader>t', function() require("mini.trailspace").trim() end, {noremap = true})
+
+vim.keymap.set('n', '-', function() require("mini.files").open() end, {noremap = true})
 
 -- EndPluginKeymaps
 
